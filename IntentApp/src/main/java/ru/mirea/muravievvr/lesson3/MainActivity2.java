959685e95b26c2278366377095a0820bf9dc46f2
Murken-0 <muravievvlad.2002@gmail.com>
2023-03-22 14:32:00 +0300

    add intentapp module + sharer module

--- IntentApp/src/main/java/ru/mirea/muravievvr/lesson3/MainActivity2.java
+++ IntentApp/src/main/java/ru/mirea/muravievvr/lesson3/MainActivity2.java
@@ -2,7 +2,9 @@ package ru.mirea.muravievvr.lesson3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        TextView textView = findViewById(R.id.textView);
+        String text = "КВАДРАТ ЗНАЧЕНИЯ МОЕГО НОМЕРА ПО СПИСКУ В ГРУППЕ СОСТАВЛЯЕТ 441,\n " +
+                "а текущее время " + intent.getStringExtra("date");
+        textView.setText(text);
     }
 }
